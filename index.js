28a73ec98e6a9d1c75532ea59e79767743682faf
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-31 09:37:45 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (drivers, str) => {
+    return drivers.filter(driver => driver.toLowerCase().includes(str.toLowerCase()));
+  };
+  
+  const fuzzyMatch = (drivers, str) => {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(str.toLowerCase()));
+  };
+  
+  const matchName = (drivers, str) => {
+    return drivers.filter(driver => driver.name.toLowerCase().includes(str.toLowerCase()));
+  };
